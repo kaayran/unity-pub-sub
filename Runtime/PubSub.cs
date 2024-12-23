7a6770d79a223a72b586defe2e1dc2c7aef813a1
kaayran <dodororo132@gmail.com>
2024-12-24 00:27:14 +0400

    moved to Laudem.PublisherSubscriber namespace

--- Runtime/PubSub.cs
+++ Runtime/PubSub.cs
@@ -1,45 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public static class PubSub
+namespace Laudem.PublisherSubscriber
 {
-    public static void Subscribe(string message, Delegate callback)
+    public static class PubSub
     {
-        if (!_subscriptions.ContainsKey(message))
+        public static void Subscribe(string message, Delegate callback)
         {
-            _subscriptions[message] = new List<Delegate>();
-        }
-
-        _subscriptions[message].Add(callback);
-    }
-
-    public static void Unsubscribe(string message, Delegate callback)
-    {
-        if (!_subscriptions.ContainsKey(message)) return;
-
-        _subscriptions[message].Remove(callback);
-        if (_subscriptions[message].Count == 0)
-        {
-            _subscriptions.Remove(message);
-        }
-    }
-
-    public static void Broadcast(string message)
-    {
-        if (!_subscriptions.TryGetValue(message, out var subscription)) return;
-
-        foreach (var callback in subscription)
-        {
-            if (callback is Action action)
+            if (!_subscriptions.ContainsKey(message))
             {
-                action();
+                _subscriptions[message] = new List<Delegate>();
+            }
+
+            _subscriptions[message].Add(callback);
+        }
+
+        public static void Unsubscribe(string message, Delegate callback)
+        {
+            if (!_subscriptions.ContainsKey(message)) return;
+
+            _subscriptions[message].Remove(callback);
+            if (_subscriptions[message].Count == 0)
+            {
+                _subscriptions.Remove(message);
             }
         }
+
+        public static void Broadcast(string message)
+        {
+            if (!_subscriptions.TryGetValue(message, out var subscription)) return;
+
+            foreach (var callback in subscription)
+            {
+                if (callback is Action action)
+                {
+                    action();
+                }
+            }
+        }
+
+        #region private
+
+        private static readonly Dictionary<string, List<Delegate>> _subscriptions = new();
+
+        #endregion
     }
-
-    #region private
-
-    private static readonly Dictionary<string, List<Delegate>> _subscriptions = new();
-
-    #endregion
 }
